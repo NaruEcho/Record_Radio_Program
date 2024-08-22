64eb93eecad981775be2c076389e5de67d78db8e
ShiningWank0 <hogehogejak@gmail.com>
2024-08-23 08:37:22 +0900

    Update radio_eikaiwa.py

--- scripts/radio_eikaiwa.py
+++ scripts/radio_eikaiwa.py
@@ -4,24 +4,24 @@ import pytz
 # 日本時間のタイムゾーンを設定
 JST = pytz.timezone('Asia/Tokyo')
 
-# 現在の日本時間の時刻、日付、年を取得
+# 現在の日本時間の年、月、日、曜日を取得
 now = datetime.now(JST)
-current_time = now.strftime('%H:%M:%S')
-current_date = now.strftime('%Y-%m-%d')
-current_year = now.year
 
-# 一週間前の最終平日を取得する関数
+# らじるらじるの放送日取得
 def get_last_week(now_date):
-    time_delta = 0
-    if( now_date.weekday > 4 ):
-        time_delta = now_date.weekday - 4
-    last_week_weekday = now - timedelta(days=7) - timedelta(days=time_delta)
+    last_week = now_date - timedelta(days=7)
+    if last_week.weekday() > 4:
+        last_week -= timedelta(days=(last_week.weekday() - 4))
     weekdays = []
-    weekdays.append(last_week_weekday)
-    for i in range(3):
-        if( last_week_weekday.weekday == 4 ):
-            next_weekday = last_week_day + timedelta(days=3)
+    weekdays.append(last_week)
+    for i in range(4):
+        # もし一週間前の最終平日が金曜日なら次の月曜日の日付を取得する
+        if last_week.weekday() == 4:
+            weekdays.append(last_week + timedelta(days=3))
+        # 一週間前の最終平日が金曜日以外ならその翌日の日付を取得する
         else:
-            next_weekday = last_week_day + timedelta(days=1)
-        weekdays.append(next_weekday)
+            weekdays.append(last_week + timedelta(days=1))
     return weekdays
+
+for date in weekdays:
+    print(f"放送日は{date}")
