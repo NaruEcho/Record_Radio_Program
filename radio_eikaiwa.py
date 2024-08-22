19dd68e846fa5aad1a5a3b9fc12c5a8e3e0e7704
ShiningWank0 <hogehogejak@gmail.com>
2024-08-22 23:34:39 +0900

    Create radio_eikaiwa.py

---
+++ radio_eikaiwa.py
@@ -0,0 +1,19 @@
+from datetime import datetime, timedelta
+import pytz
+
+# 日本時間のタイムゾーンを設定
+JST = pytz.timezone('Asia/Tokyo')
+
+# 現在の日本時間の時刻、日付、年を取得
+now = datetime.now(JST)
+current_time = now.strftime('%H:%M:%S')
+current_date = now.strftime('%Y-%m-%d')
+current_year = now.year
+
+# 一週間前の最終平日を取得する関数
+def get_last_week(now_date):
+    time_delta = 0
+    if( now_date.weekday > 4 ):
+        time_delta = now_date.weekday - 4
+    last_week_weekday = now - timedelta(days=7) - timedelta(days=time_delta)
+    return last_week_weekday
