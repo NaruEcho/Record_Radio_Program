61cba45e5691ff09e17dec5507dfdc40817efff9
ShiningWank0 <hogehogejak@gmail.com>
2024-08-24 23:21:26 +0900

    Update record_stream_radio_gokaku.py

--- scripts/record_stream_radio_gokaku.py
+++ scripts/record_stream_radio_gokaku.py
@@ -1,10 +1,32 @@
 #!/usr/bin/env python
 import ffmpeg
+import sys
+import os
 
-url = 'https://radio-stream.nhk.jp/hls/live/2023501/nhkradiruakr2/master.m3u8'
-length = 10
-saveFile = 'test_weather.mp3'
+def record_audio(
+  url='https://radio-stream.nhk.jp/hls/live/2023501/nhkradiruakr2/master.m3u8',
+  length=10,
+  save_file_path='test_weather',
+  record_type='mp3'):
+    stream = ffmpeg.input(url, t=length)
+    stream = ffmpeg.output(stream, save_file_path, format=record_type)
+    ffmpeg.run(stream)
 
-stream = ffmpeg.input(url, t=length)
-stream = ffmpeg.output(stream, saveFile, format='mp3')
-ffmpeg.run(stream)
+def rename_audio_filename(original_file_path, file_format):
+  # オーディオ形式と拡張子のマッピング
+  format_to_extension = {
+    'mp3': '.mp3',
+    'wav': '.wav',
+    'flac': '.flac',
+    'aac': '.aac',
+    'ogg': '.ogg'
+  }
+  # 指定された形式に対応する拡張子を取得
+  extension = format_to_extension.get(file_format.lower())
+  if not extension:
+    raise ValueError("Unsupported file format.")
+  # ファイルの拡張子を取り除く
+  base_name, _ = os.path.splitext(original_file_path)
+  # 新しいファイル名を生成
+  new_file_name = f"{base_name}{extension}"
+  return new_file_name
