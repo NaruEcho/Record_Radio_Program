6bc50e5a7ebf808ffbab41572d778b3a464efc89
ShiningWank0 <hogehogejak@gmail.com>
2024-08-25 18:26:53 +0900

    Update record_stream_radio_gogaku.py

--- scripts/record_stream_radio_gogaku.py
+++ scripts/record_stream_radio_gogaku.py
@@ -9,7 +9,10 @@ def record_audio(url, length, save_file_path, record_type):
     save_file_path = rename_audio_filename(save_file_path, record_type)
     try:
         # ヘッダーを設定
-        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
+            'Referer': url
+        }
         # ストリートを取得
         response = requests.get(url, headers=headers, stream=True)
         #print(response.json())
