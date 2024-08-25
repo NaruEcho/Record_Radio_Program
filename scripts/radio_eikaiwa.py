1a713c2e4cea302a1864aa772e3e5fcaa34a3b7a
ShiningWank0 <hogehogejak@gmail.com>
2024-08-25 15:07:37 +0900

    Update radio_eikaiwa.py

--- scripts/radio_eikaiwa.py
+++ scripts/radio_eikaiwa.py
@@ -5,35 +5,6 @@ import argparse
 import subprocess
 import math
 
-"""
-# らじるらじるの放送日取得
-def get_last_weekdays(now_date):
-    last_week = now_date - timedelta(days=7)
-    if last_week.weekday() > 4:
-        last_week -= timedelta(days=(last_week.weekday() - 4))
-    weekdays = []
-    for _ in range(5):
-        weekdays.append([last_week.year, last_week.month, last_week.day])
-        # もし一週間前の最終平日が金曜日なら次の月曜日の日付を取得する
-        if last_week.weekday() == 4:
-            last_week += timedelta(days=3)
-        # 一週間前の最終平日が金曜日以外ならその翌日の日付を取得する
-        else:
-            last_week += timedelta(days=1)
-    return weekdays
-
-
-for index, date in enumerate(get_last_weekdays(now)):
-    tar_path = f"content/radio_eikaiwa/date[0]/date[1]/date[2].mp3"
-    if os.path.exists(tar_path):
-        date.append(False)
-    else:
-        date.append(True)
-    # ここでボタンのidタグ検索で取得した配列でTrueの場合のみ、index番目のボタンを押して再生する処理
-    print(f"放送日は{date[0]}-{date[1]}-{date[2]}")
-"""
-
-
 def run_recording(url, length, save_file_path, record_type):
     command = [
         'python',
