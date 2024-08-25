c3394d65c51d350fe926fc9e86661e4b9fb9417f
ShiningWank0 <hogehogejak@gmail.com>
2024-08-25 10:40:23 +0900

    Update radio_eikaiwa.py

--- scripts/radio_eikaiwa.py
+++ scripts/radio_eikaiwa.py
@@ -1,6 +1,8 @@
 from datetime import datetime, timedelta
 import pytz 
 import os
+import argparse
+import subprocess
 
 # 日本時間のタイムゾーンを設定
 JST = pytz.timezone('Asia/Tokyo')
@@ -33,4 +35,55 @@ for index, date in enumerate(get_last_weekdays(now)):
         date.append(True)
     # ここでボタンのidタグ検索で取得した配列でTrueの場合のみ、index番目のボタンを押して再生する処理
     print(f"放送日は{date[0]}-{date[1]}-{date[2]}")
+
+
+def run_recording(url, length, save_file_path, record_type):
+    command = [
+        'python',
+        'record_audio.py',
+        '-u',
+        url,
+        '-l',
+        length,
+        '-s',
+        save_file_path,
+        '-rt',
+        record_type
+    ]
+    subprocess.run(command, check=True)
+
+
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(description="Record audio from a stream URL.")
+  # コマンドライン引数の定義
+  parser.add_argument("-u", 
+                      "--url", 
+                      type=str, 
+                      default='https://radio-stream.nhk.jp/hls/live/2023501/nhkradiruakr2/master.m3u8', 
+                      help="Stream URL to record from (default: NHK stream)."
+                     )
+  parser.add_argument("-l",
+                      "--length",
+                      type=int,
+                      default=2,
+                      help="Length of the recording in minutes (default: 2)."
+                     )
+  parser.add_argument("-s",
+                      "--save_file_path",
+                      type=str,
+                      default='test_weather',
+                      help="File path (except extension) to save the recording (default: 'test_weather')."
+                     )
+  parser.add_argument("-rt",
+                      "--record_type",
+                      type=str,
+                      default='mp3',
+                      help="Select audio format."
+                     )
+
+# コマンドライン引数の解析
+args = parser.paese_args()
+
+# 録音を実行
+run_recording(args.url, args.length, args.save_file_path, args.record_type)
 
