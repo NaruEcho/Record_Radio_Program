4d30aee8a78ed727c8339d8af66634bb20c10350
ShiningWank0 <hogehogejak@gmail.com>
2024-08-26 10:12:26 +0900

    Create set_sound.sh

---
+++ scripts/set_sound.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -xeo pipefail
+
+# パッケージの更新とインストール
+sudo apt update
+sudo apt install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
+
+# PulseAudioサービスの再起動
+systemctl --user restart pulseaudio.service
+systemctl --user restart pulseaudio.socket
+
+# 仮想オーディオデバイスの設定
+pactl load-module module-null-sink sink_name=auto_null sink_properties=device.description="Dummy Output"
+pactl load-module module-remap-sink sink_name=auto_null_remap master=auto_null
+
+# デバイスのリスト表示（デバッグ用）
+pactl list
