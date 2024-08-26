f6fde7f2f103348850bc0259d92e1f55a972efa0
ShiningWank0 <hogehogejak@gmail.com>
2024-08-26 10:27:18 +0900

    Update set_audio.sh

--- scripts/set_audio.sh
+++ scripts/set_audio.sh
@@ -6,11 +6,14 @@ sudo apt update
 sudo apt install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
 
 # PulseAudioサービスの再起動
-systemctl --user restart pulseaudio.service
-systemctl --user restart pulseaudio.socket
+pulseaudio --kill || true
+pulseaudio --start
+
+# 再起動後の遅延を追加
+sleep 10
 
 # 仮想オーディオデバイスの設定
-pactl load-module module-null-sink sink_name=auto_null sink_properties=device.description="Dummy Output"
+pactl -v load-module module-null-sink sink_name=auto_null sink_properties=device.description="Dummy Output"
 pactl load-module module-remap-sink sink_name=auto_null_remap master=auto_null
 
 # デバイスのリスト表示（デバッグ用）
