a8db654a86663797f25c90c2101cee92daf18f0c
ShiningWank0 <hogehogejak@gmail.com>
2024-08-28 00:48:23 +0900

    Delete scripts/Linux_set_audio.sh

--- scripts/Linux_set_audio.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -xeo pipefail
-
-sudo apt update
-sudo apt install -y pulseaudio libportaudio2 dbus-x11 libasound-dev
-systemctl --user restart pulseaudio.service
-systemctl --user restart pulseaudio.socket
-pactl list
