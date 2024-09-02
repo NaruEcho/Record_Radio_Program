47a43dc8eb3df7eff6407e30d5b81d243689dc8b
ShiningWank0 <hogehogejak@gmail.com>
2024-09-03 08:12:44 +0900

    Delete sample directory

--- sample/rfriends3_latest_script/rfriends3/script/ans.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem Rfriends (radiko radiru˜^‰¹ƒc[ƒ‹)
-set /p ans1=""
-if "%ans1%"=="" (
-	echo.
-) else (
-	echo %ans1%
-)
--- sample/rfriends3_latest_script/rfriends3/script/rf_clear.php
+++
@@ -1,2 +0,0 @@
-<?php
- function rf_clear() { global $base; global $scrdir; global $htmldir; $clist = array( $htmldir."phpinfo.html", $scrdir."config_sys_07.php", $scrdir."config_sys_08.php" ); foreach ($clist as $cl) { if (file_exists($cl)) { unlink($cl); } } $files = $scrdir."*.bak"; foreach (glob($files) as $val ) { unlink($val); } } 
