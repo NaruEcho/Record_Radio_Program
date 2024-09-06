db18359113e13ba6dc1445fc09226f08a599c253
ShiningWank0 <hogehogejak@gmail.com>
2024-09-06 21:58:12 +0900

    Create generate_temp_json.py

---
+++ scripts/generate_temp_json.py
@@ -0,0 +1,9 @@
+import json
+
+# JSONファイルの読み込み
+with open('content/courses-all.json.json', 'r', encoding='utf-8') as file:
+    courses_json = json.load(file)
+
+# ダウンロードする番組を配列形式で受け取る
+with open('content/programs.txt', 'r', encoding='utf-8') as file:
+    programs_list = file.read().splitlines()
