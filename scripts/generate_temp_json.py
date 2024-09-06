b6423d594e8cc162ad6e1599568f0f3b36d09789
ShiningWank0 <hogehogejak@gmail.com>
2024-09-06 22:00:20 +0900

    Update generate_temp_json.py

--- scripts/generate_temp_json.py
+++ scripts/generate_temp_json.py
@@ -7,3 +7,6 @@ with open('content/courses-all.json.json', 'r', encoding='utf-8') as file:
 # ダウンロードする番組を配列形式で受け取る
 with open('content/programs.txt', 'r', encoding='utf-8') as file:
     programs_list = file.read().splitlines()
+
+for program in courses_json['programs']:
+    if program['title'] in programs_text
