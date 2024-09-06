06971ea549ebadeaec8438138cdc15c1394ba5dc
ShiningWank0 <hogehogejak@gmail.com>
2024-09-06 22:27:24 +0900

    Update generate_temp_json.py

--- scripts/generate_temp_json.py
+++ scripts/generate_temp_json.py
@@ -8,5 +8,21 @@ with open('content/courses-all.json.json', 'r', encoding='utf-8') as file:
 with open('content/programs.txt', 'r', encoding='utf-8') as file:
     programs_list = file.read().splitlines()
 
+output_list = []
+
 for program in courses_json['programs']:
-    if program['title'] in programs_text
+    if program['title'] in programs_list:
+        url = courses_json['url_json'].format(program['dir'], program['sub'])
+        output_list.append({
+            "title": program['title'],
+            "url": url,
+            "folder_title": program['folder_title']
+        })
+
+# json形式で出力
+output_json = json.dumps(output_list, ensure_ascii=False, indent=2)
+print(output_json)
+
+# ファイル保存
+with open("temp.json", "w", encoding="utf-8") as f:
+    f.write(output_json)
