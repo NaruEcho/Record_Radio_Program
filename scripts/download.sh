7c6c34f38edbb268b0a8daf5f3d586eea2f2d448
ShiningWank0 <hogehogejak@gmail.com>
2024-09-08 15:36:16 +0900

    Create download.sh

---
+++ scripts/download.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# JSONファイルのパス
+json_file="scripts/temp.json"
+
+# jqコマンドが必要です
+if ! command -v jq &> /dev/null; then
+    echo "jq command not found. Install jq like sudo apt-get install -y jq"
+    exit 1
+fi
+
+# JSONファイルを読み込んで、各エントリに対して処理を実行
+jq -c '.[]' "$json_file" | while IFS= read -r entry; do
+    url=$(echo "$entry" | jq -r '.streaming_url')
+    path=$(echo "$entry" | jq -r '.audio_path')
+
+    if [[ -n "$url" && -n "$path" ]]; then
+        echo "ffmpeg -http_seekable 0 -i \"$url\" -loglevel quiet -ab 46k -ac 2 -ar 24000 -write_xing 0 \"$path\""
+        ffmpeg -http_seekable 0 -i "$url" -loglevel quiet -ab 46k -ac 2 -ar 24000 -write_xing 0 "$path"
+    else
+        echo "Error: URL or PATH not found"
+    fi
+done
