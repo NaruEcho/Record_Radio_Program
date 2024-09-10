e73447f1b84122f0e8a5049ea59a67f4fd0f2d23
ShiningWank0 <hogehogejak@gmail.com>
2024-09-10 18:04:54 +0900

    Update transcribe.py

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -1,26 +1,28 @@
-import whisper
+from faster_whisper import WhisperModel
 import ffmpeg
 import pysrt
 import sys
 
-model = whisper.load_model("medium")
+model = WhisperModel("large-v3", device="cpu", compute_type="int8")
 
 audio_source = sys.argv[1]
 
-result = model.transcribe(audio_source, verbose=True)
-
-print(result["text"])
+segments, info = model.transcribe(
+	audio_source,
+	beam_size=5,
+	vad_filter=True,
+	without_timestamps=False,)
 
 subs = pysrt.SubRipFile()
 sub_idx = 1
-
-for i in range(len(result["segments"])):
-    start_time = result["segments"][i]["start"]
-    end_time = result["segments"][i]["end"]
+	
+print("Detected language '%s' with probability %f" % (info.language, info.language_probability))
+for segment in segments:
+    start_time = segment.start
+    end_time = segment.end
     duration = end_time - start_time
     timestamp = f"{start_time:.3f} - {end_time:.3f}"
-    text = result["segments"][i]["text"]
-    
+    text = segment.text
     sub = pysrt.SubRipItem(index=sub_idx, start=pysrt.SubRipTime(seconds=start_time), 
                            end=pysrt.SubRipTime(seconds=end_time), text=text)
     subs.append(sub)
