25aad91e383d9b7661e5db37fc316bf131039c48
ShiningWank0 <hogehogejak@gmail.com>
2024-09-11 01:57:01 +0900

    Update transcribe.py

--- scripts/transcribe.py
+++ scripts/transcribe.py
@@ -41,7 +41,7 @@ def transcribe_audio_with_silence_handling(audio_file, silence_thresh=-40, min_s
         # チャンクの再生時間を加算（len(chunk)はミリ秒なので）
         current_time += len(chunk)
 
-    os.makedir(f"temp/{os.path.dirname(audio_file)}", exist_ok=True)
+    os.makedirs(f"temp/{os.path.dirname(audio_file)}", exist_ok=True)
 
     # SRTファイルに書き出し
     with open(f"temp/{audio_file}.srt", "w", encoding='utf-8') as srt_file:
@@ -58,7 +58,7 @@ def transcribe(path):
         end_time = segment.end
         srt_entry = srt.Subtitle(index=len(srt_entries) + 1, start=start_time, end=end_time, content=segment.text)
         srt_entries.append(srt_entry)
-    os.makedir(f"temp/{os.path.dirname(audio_file)}", exist_ok=True)
+    os.makedirs(f"temp/{os.path.dirname(audio_file)}", exist_ok=True)
     with open(f"temp/{audio_file}.srt", "w", encoding='utf-8') as srt_file:
         srt_file.write(srt.compose(srt_entries))
 
