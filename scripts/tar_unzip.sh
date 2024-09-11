5c2040ca440ef350958e7d3d36c25cfe7f84eec9
ShiningWank0 <hogehogejak@gmail.com>
2024-09-11 13:14:50 +0900

    Create tar_unzip.sh

---
+++ scripts/tar_unzip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# 圧縮ファイルが保存されているフォルダ
+SOURCE_DIR="downloaded-artifacts"
+# 圧縮ファイルを移動するフォルダ
+DEST_DIR="./"
+
+# 圧縮ファイルを移動して展開する
+for file in "$SOURCE_DIR"/*.tar.gz; do
+  if [ -f "$file" ]; then
+    # ファイルを移動
+    mv "$file" "$DEST_DIR"
+    
+    # 移動後、展開
+    tar -xzf "$DEST_DIR/$(basename "$file")" -C "$DEST_DIR"
+
+    # 展開が終わったら圧縮ファイルを削除
+    rm "$DEST_DIR/$(basename "$file")"
+    
+    echo "展開完了: $file"
+  fi
+done
